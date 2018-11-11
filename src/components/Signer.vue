8f0296bba8ba6f854b5f465302e5f756c2863536
dkenna <dingo.mckenna@gmail.com>
2018-11-11 17:02:50 +0100

    initial

---
+++ src/components/Signer.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="Signer">
+    <h1>{{ msg }}</h1>
+      <ul v-if="users && users.length">
+        <li v-for="user of users">
+          <p><strong>{{users.title}}</strong></p>
+          <p>{{user.username}}</p>
+        </li>
+      </ul>
+
+      <ul v-if="errors && errors.length">
+        <li v-for="error of errors">
+          {{error.message}}
+        </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Signer',
+  data () {
+    return {
+      users: [],
+      errors: [],
+      msg: 'Signer'
+    }
+  },
+  created () {
+    axios.get(`http://localhost:8000/users/?format=json`)
+      .then(response => {
+        // JSON responses are automatically parsed.
+        this.users = response.data
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
+  }
+}
+</script>
