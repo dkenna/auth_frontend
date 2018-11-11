c7038339c2bf0b0defeb068ba134e65dcce92d12
dkenna <dingo.mckenna@gmail.com>
2018-11-11 19:31:59 +0100

    added sign button

--- src/components/Signer.vue
+++ src/components/Signer.vue
@@ -1,25 +1,19 @@
 <template>
-  <div class="Signer">
+  <div class="TokenSigner">
     <h1>{{ msg }}</h1>
-      <b-dropdown  v-if="users && users.length" id="ddown1" text="Dropdown Button" class="m-md-2">
-        <b-dropdown-item  v-for="user of users"
-                            :key="user.id"
-                            :value="user.username">
-         {{user.username}}
+      <b-dropdown  v-if="profiles && profiles.length"
+                     id="ddown1" text="Select User" class="m-md-2"
+                     v-model="selectedProfile">
+        <b-dropdown-item  v-for="profile of profiles"
+                            :key="profile.user.id"
+                            :value="profile.user.username"
+                            @click="selectedProfile = profile">
+         {{profile.user.username}}
         </b-dropdown-item>
       </b-dropdown>
-      <!--<ul v-if="users && users.length">
-        <li v-for="user of users">
-          <p><strong>{{users.title}}</strong></p>
-          <p>{{user.username}}</p>
-        </li>
-      </ul>
-
-      <ul v-if="errors && errors.length">
-        <li v-for="error of errors">
-          {{error.message}}
-        </li>
-      </ul>-->
+      <div><b-button>Sign and Send Token</b-button></div>
+      <div v-if="selectedProfile">User: {{selectedProfile.user.username}}</div>
+      <pre v-if="selectedProfile">{{selectedProfile.private_key}}</pre>
   </div>
 </template>
 
@@ -30,16 +24,17 @@ export default {
   name: 'Signer',
   data () {
     return {
-      users: [],
+      profiles: [],
+      selectedProfile: null,
       errors: [],
-      msg: 'Signer'
+      msg: 'TokenSigner'
     }
   },
   created () {
-    axios.get(`http://localhost:8000/users/?format=json`)
+    axios.get(`http://localhost:8000/profiles/?format=json`)
       .then(response => {
         // JSON responses are automatically parsed.
-        this.users = response.data
+        this.profiles = response.data
       })
       .catch(e => {
         this.errors.push(e)
